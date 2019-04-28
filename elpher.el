b0db00a75efe7a58126eca590a7e50322ccd123f
Tim Vaughan <tgvaughan@gmail.com>
2019-04-28 18:01:00 +0200

    Added full package header.
    Package lint now reports no issues.

--- elpher.el
+++ elpher.el
@@ -1,10 +1,36 @@
-;;; elpher.el --- elisp gopher client
+;;; elpher.el --- Full-featured gopher client.
+
+;; Copyright (C) 2019 Tim Vaughan
+
+;; Author: Tim Vaughan <tgvaughan@gmail.com>
+;; Created: 11 April 2019
+;; Version: 1.0.0
+;; Keywords: comm gopher
+;; Homepage: https://github.com/tgvaughan/elpher
+;; Package-Requires: ((emacs "25"))
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
-;; An elisp gopher client.
+;; Elpher is a tool for exploring "gopherspace" using GNU Emacs.
 
 ;;; Code:
+
+(provide 'elpher)
 
 ;;; Global constants
 ;;
@@ -20,7 +46,7 @@
    'identity
    (list "i\tfake\tfake\t1"
          "i--------------------------------------------\tfake\tfake\t1"
-         "i          Elpher Gopher Client             \tfake\tfake\t1"
+         "i           Elpher Gopher Client             \tfake\tfake\t1"
          (format "i              version %s\tfake\tfake\t1" elpher-version)
          "i--------------------------------------------\tfake\tfake\t1"
          "i\tfake\tfake\t1"
