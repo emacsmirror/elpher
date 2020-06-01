22090ec934e83be9de721db49bce21023c7d1270
Tim Vaughan <plugd@thelambdalab.xyz>
2020-06-01 22:46:47 +0200

    Version bump.

--- elpher.el
+++ elpher.el
@@ -4,7 +4,7 @@
 
 ;; Author: Tim Vaughan <plugd@thelambdalab.xyz>
 ;; Created: 11 April 2019
-;; Version: 2.7.8
+;; Version: 2.7.9
 ;; Keywords: comm gopher
 ;; Homepage: http://thelambdalab.xyz/elpher
 ;; Package-Requires: ((emacs "26"))
@@ -70,7 +70,7 @@
 ;;; Global constants
 ;;
 
-(defconst elpher-version "2.7.8"
+(defconst elpher-version "2.7.9"
   "Current version of elpher.")
 
 (defconst elpher-margin-width 6
