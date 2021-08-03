753056c2a33987fb834be545679607ad85884bcf
Alex Schroeder <alex@gnu.org>
2021-08-03 15:15:58 +0200

    Fill links
    Currently unhappy about it because fill-prefix is hard-coded to three
    spaces since that's the apparent width of "→ " (possibly font
    related?).

--- elpher.el
+++ elpher.el
@@ -1495,7 +1495,8 @@ treatment that a separate function is warranted."
          (display-string (elpher-gemini-get-link-display-string link-line))
          (address (elpher-address-from-gemini-url url))
          (type (if address (elpher-address-type address) nil))
-         (type-map-entry (cdr (assoc type elpher-type-map))))
+         (type-map-entry (cdr (assoc type elpher-type-map)))
+	 (fill-prefix "   "))
     (when display-string
       (insert elpher-gemini-link-string)
       (if type-map-entry
@@ -1509,7 +1510,7 @@ treatment that a separate function is warranted."
                                 'follow-link t
                                 'help-echo #'elpher--page-button-help))
         (insert (propertize display-string 'face 'elpher-unknown)))
-      (insert "\n"))))
+      (newline))))
 
 (defvar elpher--gemini-page-headings nil
   "List of headings on the page.")
